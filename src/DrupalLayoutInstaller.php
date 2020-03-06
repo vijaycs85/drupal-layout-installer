f5c48cd6e31caa931d8264cf4bc07523d6924cef
Vijaya Chandran Mani <vijaycs85@gmail.com>
2020-03-06 19:02:29 +0000

    Install path

--- src/DrupalLayoutInstaller.php
+++ src/DrupalLayoutInstaller.php
@@ -3,9 +3,17 @@
 namespace Vijaycs85\Composer;
 
 use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
 
 class DrupalLayoutInstaller extends LibraryInstaller
 {
+  /**
+   * {@inheritDoc}
+   */
+  public function getInstallPath(PackageInterface $package)
+  {
+    return 'layouts/';
+  }
 
   /**
    * {@inheritDoc}
